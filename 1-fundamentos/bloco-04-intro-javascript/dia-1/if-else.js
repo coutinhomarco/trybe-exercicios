d671b17eaaa9332c78ef6c369b5e9f01d1d72a22
Marco <marquinco@outlook.com>
2021-07-28 18:29:04 -0300

    else e if

---
+++ 1-fundamentos/bloco-04-intro-javascript/dia-1/if-else.js
@@ -0,0 +1,11 @@
+let notaAluno = 60;
+if (notaAluno>=80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+else if (notaAluno<80 && notaAluno >= 60) {
+    console.log("Você está na nossa lista de espera")
+
+}
+else{
+    console.log("Você foi reprovada(o)")
+}
