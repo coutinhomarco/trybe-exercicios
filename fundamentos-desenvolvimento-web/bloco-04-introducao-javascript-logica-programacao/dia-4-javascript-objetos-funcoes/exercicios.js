58609f96c06d3a07adc058e05b827452267466ff
Marco <marquinco@outlook.com>
2021-08-02 15:23:36 -0300

    adiciona exercicios for-in

--- fundamentos-desenvolvimento-web/bloco-04-introducao-javascript-logica-programacao/dia-4-javascript-objetos-funcoes/exercicios.js
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-javascript-logica-programacao/dia-4-javascript-objetos-funcoes/exercicios.js
@@ -26,3 +26,29 @@ console.log("A jogadora " +player.name +' '+player.lastName +" foi eleita a melh
 
 //Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
 console.log("A jogadora possui " +player.medals.golden+" medalhas de ouro e "+ player.medals.silver +" medalhas de prata.")
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////FOR-IN
+
+// Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+for (let index in names) {
+    console.log('Olá ' + names[index] );
+}
+  
+
+//Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+
+for (let key in car) {
+    console.log(key, car[key]);
+}
