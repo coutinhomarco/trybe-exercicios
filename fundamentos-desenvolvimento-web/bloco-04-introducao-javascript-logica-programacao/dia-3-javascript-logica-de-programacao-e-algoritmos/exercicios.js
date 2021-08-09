0149cc406468eb160a11bf427d8c1c89723704f6
Marco <marquinco@outlook.com>
2021-08-09 20:53:17 -0300

    adiciona pasta projetos

--- fundamentos-desenvolvimento-web/bloco-04-introducao-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos-desenvolvimento-web/bloco-04-introducao-javascript-logica-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,38 +1,76 @@
-const n = 5;
+const n = 7;
 let s = '*'
 let b = ' '
 
 /////////////////////////////
 // //quadrado
 
-for (let index = 1; index<= n; index+= 1) {
-    console.log(s.repeat(n))
-}
+// for (let index = 1; index<= n; index+= 1) {
+//     console.log(s.repeat(n))
+// }
 
 
 /////////////////////////////
 // //triangulo
 
-for (let index = 1; index<= n; index+= 1) {
-    console.log(s.repeat(index))
-}
+// for (let index = 1; index<= n; index+= 1) {
+//     console.log(s.repeat(index))
+// }
 
 
 /////////////////////////////
 // //triangulo invertido
 
-for (let index = 1; index<=n; index+=1) {
-    console.log(b.repeat(n-index),s.repeat(index));
-}
+// for (let index = 1; index<=n; index+=1) {
+//     console.log(b.repeat(n-index),s.repeat(index));
+// }
 
 
 /////////////////////////////
 //piramide
 
-for (let index = 1;index<=5;index+=2) {
-    let teste = (n-index)/2
-    if (teste < 1){
-        teste = 0
+// for (let index = 1;index<=5;index+=2) {
+//     let teste = (n-index)/2
+//     if (teste < 1){
+//         teste = 0
+//     }
+//     console.log(b.repeat(teste),s.repeat(index),b.repeat(teste));
+// }
+
+
+/////////////////////////////
+// BONUS
+
+
+// for (let index = 0; index<=n ; index+=2) {
+//     teste2 = (n-index)/2
+//     if (index>1) {
+//         console.log(b.repeat(teste2),'*',b.repeat(index-2),'*',b.repeat(teste2))
+//         if(index=n) {
+//             console.log(s.repeat(n));
+
+//         }
+
+//     }
+//     else{
+//         console.log(b.repeat(teste2),'*',b.repeat(teste2));
+//     }
+// }
+
+
+
+
+
+for (let index = 1; index <= n; index+=2) {
+    let aa = (n-index)/2
+    if (index === n ) {
+        console.log(b.repeat(aa),s.repeat(n))
     }
-    console.log(b.repeat(teste),s.repeat(index),b.repeat(teste));
+    else if (index>=3) {
+        console.log(b.repeat(aa),'*',b.repeat(index-2),'*');
+
+    }
+    else {
+        console.log(b.repeat(aa),s.repeat(1),b.repeat(aa))
+    }
 }
